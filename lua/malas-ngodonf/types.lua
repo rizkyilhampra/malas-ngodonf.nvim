ceaaea7b70e3245409945e5d09249221ca63868f
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-25 13:33:16 +0800

    chore(types): changed dir on comments

--- lua/malas-ngodonf/types.lua
+++ lua/malas-ngodonf/types.lua
@@ -2,14 +2,14 @@
 --- This is a simple "definition file" (https://luals.github.io/wiki/definition-files/),
 --- the @meta tag at the top is its hallmark.
 
--- lua/base/init.lua -----------------------------------------------------------
+-- lua/malas-ngodonf/init.lua -----------------------------------------------------------
 
 ---@class MalasNgodonf
 ---@field setup function: setup the plugin
 ---@field hello function: Say hello to the user
 ---@field bye function: Say bye to the user
 
--- lua/base/config.lua ---------------------------------------------------------
+-- lua/malas-ngodonf/config.lua ---------------------------------------------------------
 
 ---@class Config
 ---@field defaults Options: default options
